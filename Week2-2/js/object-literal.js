2ec4a69f16e040d8d03166c5c3a5c8f23aa409c3
moquero <moquero7@gmail.com>
2015-11-30 20:02:46 +0700

    Hotel Assignment

---
+++ Week2-2/js/object-literal.js
@@ -0,0 +1,43 @@
+var hotel = {
+
+name: 'Moquero Hotel',
+rooms: 40,
+booked: 25,
+checkAvailability : function() {
+return this.rooms - this.booked;
+}
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
+
+
+
+var hotel = new Object();
+
+hotel.name = 'Moquero Hotel'
+hotel.rooms = 40;
+hotel.booked = 25;
+
+hotel.checkAvailability = function(){
+   return this.rooms - this.booked;
+};
+
+
+function Hotel(name, rooms, booked){
+
+this.name = name;
+this.rooms = rooms;
+this.booked = booked;
+
+this.checkAvailability = function(){
+  return this.rooms - this.booked;
+};
+}
+
+
+var MoqueroHotel = new Hotel ('Moquero Hotel', 40, 25);
+var parkHotel = new Hotel ('Park', 120, 77);
--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,11 +1,38 @@
-var price;
-var quantity;
-var total;
+var products = ["Stamford T-Shirt", "Stamford Notebook", "Stamford Wristband"];
+var price = [10,5,1];
+var total = 0;
+dis = 0;
+var i = 0;
 
-price= 5;
-quantity = 14;
-total = price * quantity;
+var productList = document.getElementById("productList");
+var date = document.getElementById("date");
+var myDate = new Date();
 
-document.write("Total is " + total);
-var totalPriceEle = document.getElementById("totalPrice");
-totalPriceEle.textContent = "$" + total;
+	if ( myDate.getHours() < 12 ) {
+		date.textContent = "Good Morning!"; 
+    }
+    else  if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 ) { 
+    	date.textContent = "Good Afternoon!"; 
+    } 
+    else if ( myDate.getHours() > 17 && myDate.getHours() <= 24 ) {
+        date.textContent = "Good Evening!"; 
+    }
+    
+	while (i<3){
+		productList.innerHTML += "<li>" + products[i] + ": $" + price[i] + "</li>";
+			i++;
+	}
+
+	total = price[0] + price[1] + price[2];
+
+	dis = total * 0.8;
+
+	var totalPriceEle = document.getElementById("totalPrice");
+	totalPriceEle.textContent = "$" + dis;
+
+	var nameEle = document.getElementById("name");
+	nameEle.textContent = "Moquero";
+
+function myFunction() {
+    alert("Thank you for shopping with us!");
+    }
